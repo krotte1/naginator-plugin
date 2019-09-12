1895f745828f0bec3adb6a3cffe6e071949a2118
Kyle Rotte <krotte1@yahoo.com>
2019-09-12 17:08:58 -0600

    JENKINS-37076 initial add of test

--- src/test/java/com/chikli/hudson/plugin/naginator/NaginatorCauseTest.java
+++ src/test/java/com/chikli/hudson/plugin/naginator/NaginatorCauseTest.java
@@ -25,7 +25,6 @@
 package com.chikli.hudson.plugin.naginator;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
